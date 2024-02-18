96cae392d8dc7d56ca71c3a5f139682ed38f6918
Jason <jasonyao.jy@gmail.com>
2024-02-18 15:42:16 -0500

    Add support to Boolean literals

--- code/code.go
+++ code/code.go
@@ -17,6 +17,8 @@ const (
 	OpSub
 	OpMul
 	OpDiv
+	OpTrue
+	OpFalse
 )
 
 type Definition struct {
@@ -34,6 +36,8 @@ var definitions = map[Opcode]*Definition{
 	OpSub: {"OpSub", []int{}},
 	OpMul: {"OpMul", []int{}},
 	OpDiv: {"OpDiv", []int{}},
+	OpTrue: {"OpTrue", []int{}},
+	OpFalse: {"OpFalse", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
