4e08d1b0b003bd3b6436b998893cf19ee14aed08
Jason Yao <jasonyao105025@gmail.com>
2024-03-12 20:15:17 -0400

    Create a function to benchmark the performance of both the VM and the interpreter compared to the interpreter

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -32,7 +32,7 @@ func runVmTests(t *testing.T, tests []vmTestCase) {
 		if err != nil {
 			t.Fatalf("compiler error: %s", err)
 		}
-		 
+
 		for i, constant := range comp.Bytecode().Constants {
 			fmt.Printf("CONSTANT %d %p (%T):\n", i, constant, constant)
 
@@ -753,6 +753,30 @@ func TestRecursiveFunctions(t *testing.T) {
 			wrapper();
 			`,
 			expected: 0,
+		},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestRecursiveFibonacci(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let fibonacci = fn(x) {
+				if (x == 0) {
+					return 0;
+				} else {
+					if (x == 1) {
+						return 1;
+				} else {
+					fibonacci(x - 1) + fibonacci(x - 2);
+					}
+				}
+			};
+			fibonacci(15);
+			`,
+			expected: 610,
 		},
 	}
 
